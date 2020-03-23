6b4441ed27e7155713be58e4346a8d6353700fb4
NikolasProst <ramanik32@gmail.com>
2020-03-23 09:29:36 +0300

    Test

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -2,6 +2,7 @@ package com;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Test");
         if (args.length > 0) {
             MyCompany company = MyCompany.getInstance(args[0]);
             System.out.println(company.toString());
